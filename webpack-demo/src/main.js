d30fe7c2d20e3e3282518e9f6967377d98c2712e
linglingxx <13971385704@163.com>
2019-01-27 22:02:39 +0800

    webpack

---
+++ webpack-demo/src/main.js
@@ -0,0 +1,23 @@
+// main.js 是项目的入口文件
+
+// 导入 Jquery
+// import *** from *** 是ES6中导入模块的方式; ES6代码太高级浏览器无法解析
+import $ from 'jquery'
+
+// 使用 import 语法, 导入 css样式表
+import './css/index.css'
+import './css/index.less'
+import './css/index.scss'
+// webpack 默认只能打包处理 JS 类型的文件, 无法处理其他的非 JS 类型的文件,必须手动安装合适的第三方 loader 加载器
+
+$(function () {  
+    $('li:odd').css('backgroundColor', 'pink')
+    $('li:even').css('backgroundColor', function(){
+        return '#' + '666'
+    })
+})
+
+// webpack 能够处理 JS 文件的相互依赖关系
+// webpack 能够处理 JS 的兼容问题, 把 高级的,浏览器无法识别的语法转为低级的,浏览器能识别的语法
+// 运行的命令格式: webpack  要打包的文件的路径  -o  打包好的输出文件的路径
+// webpack-dev-server 把打包好的文件,以虚拟的形式托管到了项目的根目录中,虽然看不到,但是可以认为和 dist src node_modules 平级,有一个看不到的文件叫 bundle.js
---
+++ webpack-demo/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+// 启用热更新的 第二步
+const webpack = require('webpack')
+// 导入 在内存中生成的 HTML 页面的插件 html-webpack-plugin
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
+// 这个配置文件是一个JS文件,通过 Node 中的模块操作向外暴露了一个 配置对象
+module.exports = {
+    // 在配置文件中, 指定 入口 和 出口
+    entry: './src/main.js',  //entry 是指定打包文件的入口,可以用相对路径
+    output: {  // output 是输出的目录, 只能用绝对路径
+        path: path.join(__dirname, './dist'), //指定 打包好的文件, 输出到哪个目录中去
+        filename: 'bundle.js'  // 指定 输出的文件的名称
+    },
+    devServer: { // 配置 dev-server 命令参数的第二种形式,要麻烦一些
+        //  --open --port 3000 --contentBase ./src --hot
+        open: true, // 自动打开浏览器
+        port: 3000, // 设置启动时的运行端口号
+        contentBase: './src', // 指定托管的根目录
+        hot: true // 启用热更新HMR的 第一步
+    },
+    plugins: [ // 配置插件的节点
+        // 装了插件表示当前项目有资格开启HMR, 启用热更新第三步
+        new webpack.HotModuleReplacementPlugin(),  
+        new htmlWebpackPlugin({  // 创建一个 在内存中 生成 HTML 页面的插件
+            // 如果不传入任何配置, 默认会创建一个 index.html托管在服务器根目录, 只不过这个HTML是空的
+            template: path.join(__dirname, './src/index.html'),  // 指定模板页面,将来根据指定的页面路径,去生成内存中的页面
+            filename: 'index.html'  // 指定生成的页面的名称
+        })
+    ],
+    module: { // 症结点配置所有的 第三方模块 加载器
+        rules: [  // 所有第三方模块的 匹配规则
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            { test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader'] },
+            { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] }
+        ]
+    }
+    // mode: 'development'
+}
